679f4373247ca345a09da0e61103254a18e6db8c
taranjeet <taranjeetsinghtanu2@gmail.com>
2025-07-30 13:52:03 +0200

    Refactor app.js to include Helmet and Morgan middleware for enhanced security and logging

--- Backend/app.js
+++ Backend/app.js
@@ -6,7 +6,11 @@ dotenv.config();
 import express from "express";
 const app = express();
 import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+
 import connectToDb from "./db/db.js";
+import userRoutes from "./routes/user.route.js";
 
 // connect to MongoDb
 connectToDb();
@@ -18,8 +22,15 @@ app.use(express.urlencoded({ extended: true }));
 // enable CORS for all origins (for development purposes)
 app.use(cors());
 
+// use Helmet for securing Express apps by setting various HTTP headers
+app.use(helmet());
+// use Morgan for request logging
+app.use(morgan("dev")); // 'dev' format for logging HTTP requests
+
 app.get("/", (req, res) => {
   res.send("Hello From Express!, we are in the Backend Home Page⚒️");
 });
+
+app.use("/api/v1", userRoutes); //request will be: http://localhost:3000/api/v1/register
 
 export default app;
