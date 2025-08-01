ec0406fd6fbbd3850fb0e3ddcb0f812a9d08b361
taranjeet <taranjeetsinghtanu2@gmail.com>
2025-08-01 13:51:28 +0200

    Add getUserProfile controller and set JWT in cookie on login

--- Backend/controllers/user.controllers.js
+++ Backend/controllers/user.controllers.js
@@ -97,6 +97,8 @@ const loginUser = async (req, res, next) => {
 
     // Generate auth token
     const token = user.generateAuthToken();
+    res.cookie("token", token);
+
     res.status(200).json({
       message: "User logged in successfully",
       token,
@@ -111,7 +113,21 @@ const loginUser = async (req, res, next) => {
   }
 };
 
+const getUserProfile = async (req, res, next) => {
+  try {
+    // Here will be the logic to get user profile
+    res.status(200).json(req.user);
+  } catch (error) {
+    console.log("Error while fetching user profile", error.message);
+    res.status(500).json({
+      message: "Internal server error",
+      error: error.message || "An unexpected error occurred",
+    });
+  }
+};
+
 export default {
   registerUser,
   loginUser,
+  getUserProfile,
 };
