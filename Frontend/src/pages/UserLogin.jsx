72c52f594749014279d81f0fd6b317b54dbb5c65
taranjeet <taranjeetsinghtanu2@gmail.com>
2025-08-13 20:12:58 +0200

    feat: implement user login page with form validation, state management, and signup navigation

--- Frontend/src/pages/UserLogin.jsx
+++ Frontend/src/pages/UserLogin.jsx
@@ -1,9 +1,78 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const UserLogin = () => {
-  return (
-    <div>UserLogin</div>
-  )
-}
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [userData, setUserData] = useState({});
 
-export default UserLogin
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setUserData({
+      email,
+      password,
+    });
+    console.log(userData);
+    setEmail("");
+    setPassword("");
+  };
+
+  return (
+    <div className="p-7 flex flex-col justify-between h-screen">
+      <div>
+        <img src="/images/uber.webp" alt="uber-logo" className="w-16 mb-10" />
+
+        <form action="" onSubmit={(e) => submitHandler(e)}>
+          <h3 className="text-lg font-medium mb-2">What's your Email</h3>
+
+          <input
+            required
+            type="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            id="email"
+            placeholder="email@example.com"
+            className="bg-[#eeeeee] mb-7 rounded px-4 py-2 border w-full text-lg  placeholder:text-base"
+          />
+
+          <h3 className="text-lg font mb-2">Enter Password</h3>
+
+          <input
+            required
+            type="password"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            id="password"
+            placeholder="password"
+            className="bg-[#eeeeee] mb-7 rounded px-4 py-2 border w-full text-lg  placeholder:text-base"
+          />
+          <button className="bg-[#111] text-white font-semibold mb-3 rounded px-4 py-2 w-full text-lg  placeholder:text-base">
+            Login
+          </button>
+          <p className="text-center">
+            New here ?
+            <Link
+              to="/signup"
+              className="text-blue-600 hover:text-blue-800 ml-0.5"
+            >
+              Create a new Account
+            </Link>
+          </p>
+        </form>
+      </div>
+
+      <div>
+        <Link
+          to="/captain-login"
+          className="bg-[#10b461] flex justify-center items-center text-white font-semibold mb-5 rounded px-4 py-2 w-full text-lg  placeholder:text-base"
+        >
+          Create Account
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default UserLogin;
