eede2506ae7ee71f2de3d6598d898b5facfe1286
taranjeet <taranjeetsinghtanu2@gmail.com>
2025-09-11 23:00:37 +0200

    Create initializeCaptain method for token-based captain profile loading

--- Frontend/src/Store/useCaptainStore.js
+++ Frontend/src/Store/useCaptainStore.js
@@ -1,5 +1,6 @@
 import { create } from "zustand";
 import { devtools } from "zustand/middleware";
+import { profileOfCaptain } from "../api/captainApi";
 
 export const useCaptainStore = create(
   devtools(
@@ -7,6 +8,23 @@ export const useCaptainStore = create(
       captain: null,
       setCaptain: (captain) => set({ captain }),
       clearCaptain: () => set({ captain: null }),
+
+      // initialize CAPTAIN
+      initializeCaptain: async () => {
+        // we need to check if token is there is not token we need to stop
+        //then we need to get the profileOfCaptain
+        // then set captain with that data
+        const token = localStorage.getItem("token", token);
+        if (!token) return;
+
+        try {
+          const data = await profileOfCaptain();
+          set({ captain: data?.captain || data });
+        } catch (error) {
+          console.log("Failed to fetch Captain Profile", error);
+          set({ Captain: null });
+        }
+      },
     }),
     { name: "CaptainStore" }
   )
